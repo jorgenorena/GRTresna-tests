f49a121e464c4c4d2b742a8bc7248d24c20581b3
Jorge Noreña <jonorena@yahoo.com>
2026-01-20 08:36:01 +0000

    change matter functions to DW case

---
+++ Examples/DW_flat/MyMatterFunctions.cpp
@@ -0,0 +1,39 @@
+/* GRTresna
+ * Copyright 2024 The GRTL collaboration.
+ * Please refer to LICENSE in GRTresna's root directory.
+ */
+
+#include "ScalarField.hpp"
+
+Real ScalarField::my_potential_function(const Real &phi_here) const
+{
+    /*
+     * Symmetric potential
+     * $$
+     * V(\phi) = \frac{\lambda}{4}\left(\phi^2 - v^2\right)^2\,,
+     * $$
+    */
+    returm 0.25*m_matter_params.lambda
+        *(phi_here*phi_here - m_matter_params.v*m_matter_params.v);
+        *(phi_here*phi_here - m_matter_params.v*m_matter_params.v);
+}
+
+Real ScalarField::my_phi_function(const RealVect &loc) const
+{
+    /*
+     * Spherical domain wall
+     * $$
+     * \phi = v\tanh\left(\sqrt{\frac{\lambda}{2}}v(r - R_o))\,.
+     * $$
+     * Note that R_o is in units of the domain length
+    */
+    Real rr = sqrt(loc[0] * loc[0] + loc[1] * loc[1] + loc[2] * loc[2]);
+    Real L = domainLength[0];
+    return m_matter_params.v * tanh(sqrt(m_matter_params.lambda / 2.) * (rr - m_matter_params.R_o*L));
+}
+
+Real ScalarField::my_Pi_function(const RealVect &loc) const
+{
+    /* We just take it to be zero */
+    return 0.0;
+}
