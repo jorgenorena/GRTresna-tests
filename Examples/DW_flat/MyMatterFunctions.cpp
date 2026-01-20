c0bff3a33f93a02fbb49d544ca9f564a9ef9f658
Jorge Noreña <jonorena@yahoo.com>
2026-01-20 09:49:34 +0000

    fix bug returm

--- Examples/DW_flat/MyMatterFunctions.cpp
+++ Examples/DW_flat/MyMatterFunctions.cpp
@@ -13,7 +13,7 @@ Real ScalarField::my_potential_function(const Real &phi_here) const
      * V(\phi) = \frac{\lambda}{4}\left(\phi^2 - v^2\right)^2\,,
      * $$
     */
-    returm 0.25*m_matter_params.lambda
+    return 0.25*m_matter_params.lambda
         *(phi_here*phi_here - m_matter_params.v*m_matter_params.v);
         *(phi_here*phi_here - m_matter_params.v*m_matter_params.v);
 }
