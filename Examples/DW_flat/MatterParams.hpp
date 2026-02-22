5587fd85ed19a72a56292c90c2d74bce5516ce29
Jorge Noreña <jonorena@yahoo.com>
2026-02-22 07:28:43 +0000

    fix typo

--- Examples/DW_flat/MatterParams.hpp
+++ Examples/DW_flat/MatterParams.hpp
@@ -23,11 +23,11 @@ struct params_t
 inline void read_params(GRParmParse &pp, params_t &matter_params)
 {
     Real compactness_o_Ro;
-    pp.get("compactness_o_Ro", compactness);
+    pp.get("compactness_o_Ro", compactness_o_Ro);
     pp.get("thickness", matter_params.thickness);
     pp.get("Ro", matter_params.Ro);
 
-    matter_params.v = sqrt((3.0/32.0/M_PI)*compactness_o_Ro*thickness);
+    matter_params.v = sqrt((3.0/32.0/M_PI)*compactness_o_Ro*matter_params.thickness);
     matter_params.lambda = 2.0/(matter_params.v*matter_params.v)/(matter_params.thickness*matter_params.thickness);
 }
 
